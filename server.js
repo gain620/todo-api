4e454f848b35d573aa51a741f8fcc06b16b99b90
Gain Chang <gain930620@gmail.com>
2016-03-20 19:28:43 +0900

    refactor API with underscore module

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	desc: "Play 'Dark Souls'",
-	completed: false
-}, {
-	id: 2,
-	desc: "Call mom",
-	completed: true
-}, {
-	id: 3,
-	desc: "Feed the cat",
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 // GET root
 app.get('/', function (req, res) {
@@ -29,14 +23,14 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchTodo;
+	var matchTodo = _.findWhere(todos, {id:todoId});
 
 	// iterate todos array to find the match
-	todos.forEach(function(todo) {
-		if (todoId === todo.id) {
-			matchTodo = todo;
-		}
-	});
+	// todos.forEach(function(todo) {
+	// 	if (todoId === todo.id) {
+	// 		matchTodo = todo;
+	// 	}
+	// });
 
 	// if no id match -> send 404
 	if (matchTodo) {
@@ -45,6 +39,30 @@ app.get('/todos/:id', function(req, res) {
 		res.status(404).send('Error 404!!!');
 	}
 });
+
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	// use _.pick()
+	var body = _.pick(req.body, 'description','completed');
+
+	if(!_.isBoolean(body.completed)||!_.isString(body.description)||body.description.trim().length === 0) {
+		return res.status(400).send('Error 400!!!');
+	}
+
+	// set body.description to be trimmed value
+	body.description = body.description.trim();
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into todos
+	todos.push(body);
+
+	res.json(body);
+});
+
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!!!');
